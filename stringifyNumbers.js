c8351bfaae5d44423965c6c0fcb5eb1fa38655f6
Nikola <nikolaekmedzic@yahoo.com>
2019-09-08 18:05:53 +0200

    Add nested object algorithms

---
+++ stringifyNumbers.js
@@ -0,0 +1,40 @@
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++stringifyNumbers++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+// stringify numbers in a nested object
+
+/*
+let obj1 = {
+  num: 1,
+  test: [],
+  data: {
+      val: 4,
+      info: {
+          isRight: true,
+          random: 66
+      }
+  }
+}
+*/
+
+function stringifyNumbers (obj) {
+
+  var newObj = {};
+  
+  for (var key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } 
+    else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } 
+    else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
